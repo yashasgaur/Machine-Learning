30489e1b78d4c0033b483d3ae041782768fd13bb
Yashas Gaur <yashasgaur@gmail.com>
2026-09-03 01:33:52 +0530

    Add files via upload

---
+++ 08-decision-tree-sklearn/using_sklearn.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("BankNote_Authentication.csv")
+
+X = data[["variance", "skewness", "curtosis", "entropy"]]
+y = data["class"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+)
+
+model = DecisionTreeClassifier(max_depth=6)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy_score(y_test, y_pred)
