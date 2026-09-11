99ba738ed4ad827757445a1279878633ab2d20da
Yashas Gaur <yashasgaur@gmail.com>
2026-09-11 11:59:07 +0530

    Add files via upload

---
+++ 11-random-forest-sklearn/using_sklearn.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("WineQT-selected-columns.csv")
+X = data[["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]]
+y = data["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=24
+    )
+
+model = RandomForestClassifier(n_estimators=10, random_state=30)
+
+model.fit(X_train, y_train)
+
+model.predict(X_test)
+
+
+
